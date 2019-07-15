826bedf325b74b887f34a173855208d233c53ddd
hyerynn0521 <52299631+hyerynn0521@users.noreply.github.com>
2019-07-15 15:39:28 -0700

    Create TheGoldenRule.js

---
+++ TheGoldenRule.js
@@ -0,0 +1,8 @@
+
+function updateChatRoom() {
+  document.querySelector('.chat1').innerHTML = document.querySelector('.texty1').value;
+}
+
+function updatePrivRoom() {
+  document.querySelector('.chat2').innerHTML = document.querySelector('.texty2').value;
+}
